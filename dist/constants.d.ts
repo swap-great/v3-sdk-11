7edb13f0d65ed3b2fa2f8311420fe9f2d2b951e2
zj kkk <npckei217@gmail.com>
2024-06-25 12:48:17 +0800

    feat:update 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,6 +1,6 @@
 export declare const FACTORY_ADDRESS = "0xb4D27C618a44cF8f7300845057A643a5Af67a89a";
 export declare const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export declare const POOL_INIT_CODE_HASH = "0xd63426cf28b23363557856eaa7e085559e81e6b7794066479952bda868617297";
+export declare const POOL_INIT_CODE_HASH = "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54";
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
  */
